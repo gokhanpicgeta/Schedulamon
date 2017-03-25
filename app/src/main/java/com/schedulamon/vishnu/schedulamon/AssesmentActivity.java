722a0f04441523972abb692af5c45fe8b26b6e93
gokhanpicgeta <vpra12@student.monash.edu>
2017-03-26 03:06:11 +1100

    starting assesment_detailed

--- app/src/main/java/com/schedulamon/vishnu/schedulamon/AssesmentActivity.java
+++ app/src/main/java/com/schedulamon/vishnu/schedulamon/AssesmentActivity.java
@@ -3,11 +3,13 @@ package com.schedulamon.vishnu.schedulamon;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class AssesmentActivity extends AppCompatActivity {
+public class AssesmentActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView txtProgress;
     private ProgressBar progressBar;
@@ -22,7 +24,14 @@ public class AssesmentActivity extends AppCompatActivity {
         txtProgress = (TextView) findViewById(R.id.txtProgress);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
+        ActivitySwipeDetector activitySwipeDetector = new ActivitySwipeDetector(this);
+       RelativeLayout lowestLayout = (RelativeLayout) this.findViewById(R.id.rel);
+        lowestLayout.setOnTouchListener(activitySwipeDetector);
 
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
